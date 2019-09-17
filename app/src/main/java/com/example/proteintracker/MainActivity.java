18eec41671672ecfb3d5a773353e3b8fed02a3f7
joerickgunawan <jo.erick.gunawan160898@gmail.com>
2019-09-17 22:54:45 +0700

    First Commit

--- app/src/main/java/com/example/proteintracker/MainActivity.java
+++ app/src/main/java/com/example/proteintracker/MainActivity.java
@@ -1,10 +1,12 @@
     package com.example.proteintracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
     public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,31 @@ import android.view.View;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnPress = findViewById(R.id.btnMahasiswa);
+        btnPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frameMain1, new Mahasiswa1Fragment());
+                ft.commit();
+            }
+        });
+
+        Button btnPress2 = findViewById(R.id.btnSimpan);
+        btnPress2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.frameMain2, new Mahasiswa2Fragment());
+                ft.commit();
+            }
+        });
     }
 
         public void ToFragment(View view) {
         Intent intent = new Intent(MainActivity.this, Main3FragmentActivity.class);
         startActivity(intent);
         }
+
 }
